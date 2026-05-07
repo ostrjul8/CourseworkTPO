3918f4e6a876ddfbbbd9cc3ba0b74a7f4e63f207
ostrjul8 <studostr@gmail.com>
2026-05-07 20:34:21 +0300

    Add a parallel version of the algorithm

---
+++ CourseworkTPO/TfIdfParallel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Concurrent;
+
+namespace CourseworkTPO
+{
+    public static class TfIdfParallel
+    {
+        public static Dictionary<string, double>[] ComputeVectors(string[][] documents)
+        {
+            int documentsCount = documents.Length;
+
+            var termFrequency = new Dictionary<string, int>[documentsCount];
+
+            var documentFrequency = new ConcurrentDictionary<string, int>();
+
+            Parallel.For(0, documentsCount, i =>
+            {
+                var currentDocTf = new Dictionary<string, int>();
+                string[] words = documents[i];
+
+                foreach (string word in words)
+                {
+                    if (currentDocTf.TryGetValue(word, out int count))
+                    {
+                        currentDocTf[word] = count + 1;
+                    }
+                    else
+                    {
+                        currentDocTf[word] = 1;
+                    }
+                }
+                termFrequency[i] = currentDocTf;
+
+                foreach (string uniqueWord in currentDocTf.Keys)
+                {
+                    documentFrequency.AddOrUpdate(uniqueWord, 1, (key, oldValue) => oldValue + 1);
+                }
+            });
+
+            var tfIdfVectors = new Dictionary<string, double>[documentsCount];
+
+            Parallel.For(0, documentsCount, i =>
+            {
+                tfIdfVectors[i] = new Dictionary<string, double>();
+                var currentDocTf = termFrequency[i];
+
+                foreach (var kvp in currentDocTf)
+                {
+                    string word = kvp.Key;
+                    int termFreq = kvp.Value;
+                    int docFreq = documentFrequency[word];
+
+                    double idf = Math.Log((double)documentsCount / docFreq);
+                    tfIdfVectors[i][word] = termFreq * idf;
+                }
+            });
+
+            return tfIdfVectors;
+        }
+    }
+}
